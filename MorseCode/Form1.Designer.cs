40e7fed289c13db5433a2537145cf8c6577370ad
Anuar Kussainov <anuar.kusainov@biz.bcc.kz>
2023-02-21 18:18:31 +0600

    Shanu code

---
+++ MorseCode/Form1.Designer.cs
@@ -0,0 +1,363 @@
+﻿namespace MorseCode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(28, 53);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(234, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(28, 109);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(234, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(28, 251);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(234, 22);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(28, 307);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(234, 22);
+            this.textBox4.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(28, 157);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 28);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Зашифровать";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(28, 352);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(139, 28);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Расшифровать";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.label2.Location = new System.Drawing.Point(28, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(205, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Данные для шифровки MORSE";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.label3.Location = new System.Drawing.Point(28, 231);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(228, 16);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Данные для расшифровки MORSE";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.label4.Location = new System.Drawing.Point(313, 231);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(227, 16);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Данные для расшифровки SHANU";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.label5.Location = new System.Drawing.Point(313, 30);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(204, 16);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Данные для шифровки SHANU";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button3.Location = new System.Drawing.Point(313, 352);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(139, 28);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Расшифровать";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button4.Location = new System.Drawing.Point(313, 157);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(139, 28);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "Зашифровать";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(313, 307);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(234, 22);
+            this.textBox5.TabIndex = 13;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(313, 251);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(234, 22);
+            this.textBox6.TabIndex = 12;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(313, 109);
+            this.textBox7.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(234, 22);
+            this.textBox7.TabIndex = 11;
+            this.textBox7.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(313, 53);
+            this.textBox8.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(234, 22);
+            this.textBox8.TabIndex = 10;
+            this.textBox8.TextChanged += new System.EventHandler(this.textBox8_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.label6.Location = new System.Drawing.Point(605, 30);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(169, 16);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "Данные для шифрования";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button5.Location = new System.Drawing.Point(608, 180);
+            this.button5.Margin = new System.Windows.Forms.Padding(4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(139, 28);
+            this.button5.TabIndex = 20;
+            this.button5.Text = "Зашифровать 2";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(608, 135);
+            this.textBox9.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(234, 22);
+            this.textBox9.TabIndex = 19;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(608, 79);
+            this.textBox10.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(234, 22);
+            this.textBox10.TabIndex = 18;
+            this.textBox10.TextChanged += new System.EventHandler(this.textBox10_TextChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.label7.Location = new System.Drawing.Point(605, 53);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(121, 16);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "MORSE -> SHANU";
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button6.Location = new System.Drawing.Point(606, 336);
+            this.button6.Margin = new System.Windows.Forms.Padding(4);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(139, 28);
+            this.button6.TabIndex = 24;
+            this.button6.Text = "Зашифровать 2";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click_1);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(606, 245);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 84);
+            this.listBox1.TabIndex = 25;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.ClientSize = new System.Drawing.Size(891, 424);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "encryption box by KAB (AUPET)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ MorseCode/Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace MorseCode
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        char[] characters = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+                                                        'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 
+                                                        'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7',
+                                                        '8', '9', '0' };
+
+        string[] codeMorse = new string[] { "*–", "–***", "-*-*", "-**", 
+                                                        "*", "**-*", "--*", "****",
+                                                        "**", "*–––", "–*–", "*–**",
+                                                        "––", "–*", "–––", "*––*", 
+                                                        "--*-", "*-*", "***", "–", 
+                                                        "**–", "***-", "*--",
+                                                        "-**-", "–*––", "−−**",
+                                                        "*−−−−",
+                                                        "**−−−", "***−−", "****−",
+                                                        "*****", "−****", "−−***",
+                                                        "−−−**", "−−−−*", "−−−−−" };
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string input = textBox1.Text;
+            input = input.ToUpper();
+            string output = "";
+            int index;
+            foreach(char c in input)
+            {
+                if (c != ' ')
+                {
+                    index = Array.IndexOf(characters, c);
+                    output += codeMorse[index] + " ";
+                }
+            }
+            output = output.Remove(output.Length - 1);
+            textBox2.Text = output;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string input = textBox3.Text;
+            string[] split = input.Split(' ');
+            string output = "";
+            int index;
+            foreach (string s in split)
+            {
+                index = Array.IndexOf(codeMorse, s);
+                output += characters[index] + " ";
+            }
+            output = output.Remove(output.Length - 1);
+            textBox4.Text = output;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //аналог кода Морзе 
+        //# = *
+        //@ = -
+
+        string[] betta = new string[] {"#*", "*--", "@@-", "*@",
+                                                        "=@*", "=", "+-=", "*=",
+                                                        "@-*", "*-", "+=", "=-",
+                                                        "##-", "-++", "++@", "##@",
+                                                        "==", "++", "***", "--*",
+                                                        "@*", "+*", "===",
+                                                        "=*=*", "*+=", "*#",
+                                                        "###",
+                                                        "##*", "@@@", "---@",
+                                                        "@-@", "#-#", "*@*",
+                                                        "---", "=*", "+@#"};
+
+    
+     string[] gamma = new string[] { "#@", "@###", "@#@#", "@##",
+                                                        "#", "##@#", "@@*", "####",
+                                                        "##", "#@@@", "@*@", "#@##",
+                                                        "@@", "@#", "@@@", "#@@*",
+                                                        "@@*@", "#@#", "###", "@",
+                                                        "##@", "###@", "#@@",
+                                                        "@##@", "@#@@", "@@##",
+                                                        "#@@@@",
+                                                        "##@@@", "###@@", "###@",
+                                                        "#####", "@####", "@@###",
+                                                        "@@@##", "@@@@#", "@@@@@" };
+        
+         string[] bravo = new string[] {"1", "5", "7", "9",
+                                                        "13", "17", "21"};
+
+        char[] charli = new char[] {'A', 'B', 'C', 'D',
+                                                        'E', 'F', 'G'};
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            /*
+            string input = textBox12.Text;
+            string[] split = input.Split(' ');
+            string output = "";
+            int index;
+            foreach (string s in split)
+            {
+                index = Array.IndexOf(characters, s);
+                output += gamma[index] + " ";
+            }
+            output = output.Remove(output.Length - 1);
+            textBox11.Text = output;
+            */
+        }
+        
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string input = textBox8.Text;
+            input = input.ToUpper();
+            string output = "";
+            int index;
+            foreach (char c in input)
+            {
+                if (c != ' ')
+                {
+                    index = Array.IndexOf(characters, c);
+                    output += betta[index] + " ";
+                }
+            }
+            output = output.Remove(output.Length - 1);
+            textBox7.Text = output;
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string input = textBox6.Text;
+            string[] split = input.Split(' ');
+            string output = "";
+            int index;
+            foreach (string c in split)
+            {
+                index = Array.IndexOf(betta, c);
+                output += characters[index] + " ";
+            }
+            output = output.Remove(output.Length - 1);
+            textBox5.Text = output;
+        }
+
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string input = textBox10.Text;
+            string[] split = input.Split(' ');
+            string output = "";
+            int index;
+            foreach (string s in split)
+            {
+                index = Array.IndexOf(codeMorse, s);
+                output += betta[index] + " ";
+            }
+            output = output.Remove(output.Length - 1);
+            textBox9.Text = output;
+        }
+
+        private void textBox10_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //private void button6_Click(object sender, EventArgs e)
+        //{
+        //    string input = textBox12.Text;
+        //    string[] split = input.Split(' ');
+        //    string output = "";
+        //    int index;
+        //    foreach (string s in split)
+        //    {
+        //        index = Array.IndexOf(gamma, s);
+        //        output += characters[index] + " ";
+        //    }
+        //    output = output.Remove(output.Length - 1);
+        //    textBox11.Text = output;
+        // }
+
+        
+        private void button7_Click(object sender, EventArgs e)
+        {
+            /*
+            string input = textBox14.Text;
+            string[] split = input.Split(' ');
+            string output = "";
+            int index;
+            foreach (string c in split)
+            {
+                index = Array.IndexOf(charli, c);
+                output += bravo[index] + " ";
+            }
+            output = output.Remove(output.Length - 1);
+            textBox13.Text = output;
+            */
+        }
+
+        private void textBox11_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void button6_Click_1(object sender, EventArgs e)
+        {
+            string line;
+            StreamReader reader = new StreamReader("C:\\Users\\User\\Desktop\\MorseCode\\LOL.txt");
+            line=reader.ReadLine();
+            while(line!=null)
+            {
+                listBox1.Items.Add(line);
+                line = reader.ReadLine();
+
+            }
+            reader.Close();
+
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                textBox8.Text = listBox1.Items[i].ToString();
+            }
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
